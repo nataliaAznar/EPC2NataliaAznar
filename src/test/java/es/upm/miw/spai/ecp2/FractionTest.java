054f42ccb569f1604306dfff4035a82768a3f7a0
dmRoberto <roberto.d20@gmail.com>
2014-10-06 21:37:52 +0200

    Implementados test e interfaces para la clase Fraction. Ver ticket #4

--- src/test/java/es/upm/miw/spai/ecp2/FractionTest.java
+++ src/test/java/es/upm/miw/spai/ecp2/FractionTest.java
@@ -1,6 +1,7 @@
 package es.upm.miw.spai.ecp2;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -31,6 +32,28 @@ public class FractionTest {
     public void testDecimal() {
         assertEquals(2.0, fraction.decimal(), 10e-5);
         assertEquals(1.0, fraction2.decimal(), 10e-5);
+    }
+    
+    @Test
+    public void testIsPropia (){
+    	Fraction fraction = new Fraction(2, 4);
+    	assertTrue(fraction.isPropia());
+    }
+    
+    @Test
+    public void testIsEquivalente (){
+    	Fraction fraction = new Fraction(2, 4);
+    	Fraction fractionEquivalente = new Fraction(4, 8);
+    	
+    	assertTrue(fraction.isEquivalente(fractionEquivalente));
+    }
+    
+    @Test
+    public void testHigher (){
+    	Fraction fractionMenor = new Fraction(3, 10);
+    	Fraction fractionMayor = new Fraction(6, 10);
+    	
+    	assertEquals(fractionMayor, fractionMenor.higher(fractionMayor));
     }
 
 }
